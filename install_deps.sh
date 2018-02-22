9ba66edae257a21de59b321571efe994a6cf6452
Nicolas Dilley <nicolasdilley@gmail.com>
2018-02-22 09:12:25 +0000

    Make file changes

--- install_deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
-apt-get update
-apt-get install esl-erlang
-apt-get install elixir
