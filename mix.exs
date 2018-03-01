2d21290f54c3b3a31d042db3e8cd70dddf106a1e
Nicolas Dilley <nicolasdilley@gmail.com>
2018-03-01 11:25:13 +0000

    Problem with mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DependencyManager.Mixfile do
   def project do
     [app: :solve,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.1",
      escript: escript(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
